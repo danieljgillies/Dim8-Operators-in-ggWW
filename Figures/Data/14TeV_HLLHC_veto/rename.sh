47ccc834b5bcf0fa49c0b97c8d39ce2fc04aff9b
D Gillies <dg376@sussex.ac.uk>
2025-02-06 11:19:36 +0000

    update

--- Figures/Data/14TeV_HLLHC_veto/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Iterate over every file in the directory
-for file in *; do
-    # Check if it is a file (not a directory)
-    if [ -f "$file" ]; then
-        # Rename the file with 'mll' prepended to its name
-        mv "$file" "mll_$file"
-    fi
-done
